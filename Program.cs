47086f6ed9124a4b9c716f42ddfd892ac086166e
mhrnnoi <mehrannois20@gmail.com>
2022-09-21 14:21:49 +0430

    test : design a DataBase

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DB_Connection
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
