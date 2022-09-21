445e78ee5af99b16f507359d9c82ec11d6e2ee63
mhrnnoi <mehrannois20@gmail.com>
2022-09-21 15:42:55 +0430

    test : db simulator

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@
 
 namespace DB_Connection
 {
-    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var x = new OracleConnection("lalalala") ;
+            x.Open();
+            x.close();
+
         }
     }
 }
